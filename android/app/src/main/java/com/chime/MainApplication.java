3ee976e367d974aa03efe74f4419eaa1c203b1cd
spellew <spellew1298@gmail.com>
2018-04-15 17:02:03 -0400

    react-native-maps linked

--- android/app/src/main/java/com/chime/MainApplication.java
+++ android/app/src/main/java/com/chime/MainApplication.java
@@ -3,6 +3,7 @@ package com.chime;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.airbnb.android.react.maps.MapsPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.horcrux.svg.SvgPackage;
@@ -29,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new MapsPackage(),
             new RNDeviceInfo(),
             new VectorIconsPackage(),
             new SvgPackage(),
